7247ef3b84d018d1814081022905e4fb3c3d160a
flyguy221-ERAU <feaganj@my.erau.edu>
2024-11-08 06:35:31 -0500

    Update normalize-utils.js
    updated regex logic

--- src/utils/normalize-utils.js
+++ src/utils/normalize-utils.js
@@ -1,18 +1,16 @@
-// Helper function to normalize strings by removing emojis, converting whitespace to underscores, and changing to uppercase
 const normalizeString = (str) => {
   if (typeof str !== 'string' || !str) {
     console.warn('Warning: normalizeString received a non-string or empty value:', str);
     return ''; // Returning an empty string or a sensible default
   }
-  // Remove emojis using regex, replace whitespace with underscores, convert to uppercase, and trim
+  
+  // Remove emojis using regex, normalize to lowercase, and trim
   return str
-    .replace(/[\u{1F300}-\u{1F5FF}|\u{1F600}-\u{1F64F}|\u{1F680}-\u{1F6FF}|\u{2600}-\u{26FF}|\u{2700}-\u{27BF}]/gu, '')
-    .replace(/\s+/g, '_')
-    .toUpperCase()
+    .replace(/[\u{1F300}-\u{1F5FF}|\u{1F600}-\u{1F64F}|\u{1F680}-\u{1F6FF}|\u{2600}-\u{26FF}|\u{2700}-\u{27BF}]/gu, '') // Regex for most common emojis
+    .toLowerCase()
     .trim();
 };
 
 module.exports = {
-  normalizeUUID,
   normalizeString,
 };
